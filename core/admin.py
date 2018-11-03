e1cd5a982c9849278e3237a0b75bc5a06fa81000
Paulo Vinicius Baleeiro Ferreira <paulo.baleeiro@birmind.com.br>
2018-11-03 13:01:01 -0300

    add models to admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+
+app = apps.get_app_configs('core')
+for model_name, model in app.models.items():
+    admin.site.register(model)
+
