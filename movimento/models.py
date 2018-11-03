da296098695732c60e5f119de333e71909d7af90
Paulo Vinicius Baleeiro Ferreira <paulo.baleeiro@birmind.com.br>
2018-11-03 15:50:52 -0300

    add models to app movimento

--- movimento/models.py
+++ movimento/models.py
@@ -1,3 +1,81 @@
 from django.db import models
+from core.models import Material
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Movimento(models.Model):
+    TIPOS = (
+        ('E', 'entrada'),
+        ('S', 'saída'),
+        ('P', 'perda'),
+        ('A', 'avaria')
+    )
+    data_do_movimento = models.DateField(auto_now=True, editable=False)
+    usuário = models.ForeignKey(User, on_delete=models.PROTECT)
+    tipo_de_movimento = models.CharField(choices=TIPOS)
+
+    class Meta:
+        verbose_name = 'Movimento'
+        verbose_name_plural = 'Movimentos'
+
+
+class MovimentoMaterial(models.Model):
+    movimento = models.ForeignKey(Movimento)
+    material = models.ForeignKey(Material)
+    motivo = models.CharField(max_length=20)
+    quatidade = models.FloatField()
+
+    class Meta:
+        verbose_name = 'Movimento - material'
+        verbose_name_plural = 'Movimento-material'
+        unique_together = ['movimento', 'material']
+
+
+class Devolução(models.Model):
+    data = models.DateField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.PROTECT)
+
+
+    class Meta:
+        verbose_name = 'Devolução'
+        verbose_name_plural = "Devoluções"
+
+
+class ItemDevoucao(models.Model):
+    devolução = models.ForeignKey(Devolução, on_delete=models.PROTECT)
+    material = models.ForeignKey(Material, on_delete=models.PROTECT)
+    motivo = models.CharField(max_length=30)
+    quantidade = models.FloatField(default=0)
+
+    class Meta:
+        verbose_name = 'Item - Devoluçaõ'
+        verbose_name_plural = 'Itens - devoluções'
+        unique_together = ['devolução', 'material']
+
+
+class Requisicao(models.Model):
+    SITUAÇÃO = (
+        ('A', 'ABERTO'),
+        ('PA', 'PARCIALMENTE ATENDIDA'),
+        ('F', 'FECHADA')
+    )
+    data = models.DateField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.PROTECT)
+    situação = models.CharField(choices=SITUAÇÃO)
+
+    class Meta:
+        verbose_name = 'requisição'
+        verbose_name_plural = 'requisições'
+
+
+class RequisicaoMaterial(models.Model):
+    requisicao = models.ForeignKey(Requisicao, on_delete=models.PROTECT)
+    material = models.ForeignKey(Material, on_delete=models.PROTECT)
+    serviço = models.CharField(max_length=10, help_text='almoço, jantar, desejum...')
+    quantidade = models.FloatField(default=0)
+
+
+    class Meta:
+        verbose_name_plural = 'requisições - materiais'
+        unique_together = ['requisicao', 'material']
+
