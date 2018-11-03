3076aeb2c1a34acb0901a33de774df6ac1983172
Paulo Vinicius Baleeiro Ferreira <paulo.baleeiro@birmind.com.br>
2018-11-03 13:47:02 -0300

    add inventario to admin and create models

--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from core.models import Material
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Inventario(models.Model):
+    data = models.DateField(verbose_name='data de realização', auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+
+
+class ItemIventario(models.Model):
+    registro_de_inventario = models.ForeignKey(Inventario, on_delete=models.CASCADE)
+    material = models.ForeignKey(Material, on_delete=models.PROTECT)
+    quantidade = models.DecimalField(decimal_places=3)
