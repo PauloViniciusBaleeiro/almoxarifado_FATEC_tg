96e56db66ddc4a4ae4c014fa550135ef4201bac8
Paulo Vinicius Baleeiro Ferreira <paulo.baleeiro@birmind.com.br>
2018-11-06 21:22:11 -0200

    funções iniciais

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.conf import settings
 
-# Create your views here.
+
+def home(request):
+    return render(request, settings.BASE_DIR + '/templates/home.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def logout(request):
+    logout(request)
+    return redirect('home')
