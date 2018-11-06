801d42130589501096e143906814a1f6a45f2ab1
Paulo Vinicius Baleeiro Ferreira <paulo.baleeiro@birmind.com.br>
2018-11-06 21:21:58 -0200

    add urls ao projeto

--- TG_Fatec/urls.py
+++ TG_Fatec/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import login, logout, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
 ]
