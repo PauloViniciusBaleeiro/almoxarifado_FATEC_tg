e7b86a144b60ac438cb068b1b096d37c79ce6685
Paulo Vinicius Baleeiro Ferreira <paulo.baleeiro@birmind.com.br>
2018-11-11 19:14:13 -0200

    url para novo fabricante

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, logout, listagem_fabricante as list
+from .views import home, logout, listagem_fabricante as list, novo_fabricante as novo
 
 urlpatterns = [
     path('', home, name='home'),
     path('logout/', logout, name='logout'),
-    path('fabricantes/', list, name='list_fabricantes')
+    path('fabricantes/', list, name='list_fabricantes'),
+    path('fabricantes/novo/', novo, name='novo_fabricante'),
 ]
