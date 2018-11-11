0fba465a74a070a265a69de269414cd649db92d0
Paulo Vinicius Baleeiro Ferreira <paulo.baleeiro@birmind.com.br>
2018-11-11 13:36:00 -0200

    fix urls path

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, logout
+from .views import home, logout, listagem_fabricante as list
 
 urlpatterns = [
     path('', home, name='home'),
     path('logout/', logout, name='logout'),
+    path('fabricantes/', list, name='list_fabricantes')
 ]
