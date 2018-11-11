f99b4142d9de1dcccfcaa4ed57e0461a1be41765
Paulo Vinicius Baleeiro Ferreira <paulo.baleeiro@birmind.com.br>
2018-11-11 13:36:18 -0200

    adicionado função de listagem de fabricantes

--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from .forms import FabricanteForms
+from .models import Fabricante
+
 
 
 def home(request):
@@ -9,3 +13,16 @@ def home(request):
 def logout(request):
     logout(request)
     return redirect('home')
+
+
+@login_required
+def listagem_fabricante(request):
+    fabricantes = Fabricante.objects.all()
+    return render(request, 'list_fabricantes.html', {'fabricantes': fabricantes})
+
+@login_required
+def novo_fabricante(request):
+    form = FabricanteForms(request.POST or None, request.FILES or None)
+
+    if form.is_valid():
+        form.save()
