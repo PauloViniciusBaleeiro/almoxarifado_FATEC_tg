7abb6a31cdb555a084cae38a717bc406971ceedc
Paulo Vinicius Baleeiro Ferreira <paulo.baleeiro@birmind.com.br>
2018-11-11 11:29:38 -0200

    eliminando função login

--- core/views.py
+++ core/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
-from django.conf import settings
 
 
 def home(request):
-    return render(request, settings.BASE_DIR + '/templates/home.html')
-
-
-def login(request):
-    return render(request, 'login.html')
+    return render(request, 'home.html')
 
 
 def logout(request):
