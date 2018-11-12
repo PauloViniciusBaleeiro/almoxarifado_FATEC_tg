3f31a6ec78b01d92d507b3f2561e95e2148bcc44
Paulo Vinicius Baleeiro Ferreira <paulo.baleeiro@birmind.com.br>
2018-11-11 23:55:43 -0200

    adição de novos forms

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from .models import Fabricante
+from .models import Fabricante, Cidade, Estado, Contato
 
 
 class FabricanteForms(ModelForm):
@@ -7,3 +7,20 @@ class FabricanteForms(ModelForm):
         model = Fabricante
         fields = ['CNPJ', 'razao_social', 'nome_fantasia', 'logradouro', 'nome_do_logradouro', 'numero',
                   'complemento', 'bairro', 'cep', 'cidade']
+
+class EstadoForms(ModelForm):
+    class Meta:
+        model = Estado
+        fields = ['sigla', 'descrição']
+
+
+class CidadeForm(ModelForm):
+    class Meta:
+        model = Cidade
+        fields = ['nome', 'estado']
+
+
+class ContatoForm(ModelForm):
+    class Meta:
+        model = Contato
+        fields = ['telefone', 'e_mail']
