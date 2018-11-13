a6809b2d8bf96bff9e8ca8cf9437d1b5513014dd
Paulo Vinicius Baleeiro Ferreira <paulo.baleeiro@birmind.com.br>
2018-11-13 20:15:50 -0200

    cadastramento de novos urls

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (home, logout, listagem_fabricante as list, novo_fabricante as novo, cadastra_cidade,
-                    cadastra_estado, cadastra_contato, atualiza_fabricante, deleta_contato)
+                    cadastra_estado, cadastra_contato, atualiza_fabricante, deleta_contato, lista_material as list_m,
+                    cadastra_material, cadastra_tipo_de_material as cad_tipo_mat,)
 
 urlpatterns = [
     path('', home, name='home'),
@@ -11,5 +12,8 @@ urlpatterns = [
     path('fabricantes/contatos/<int:id>', cadastra_contato, name='contato'),
     path('fabricantes/contatos/deletar/<int:id>', deleta_contato, name='remover_contato'),
     path('cidades/nova/', cadastra_cidade, name='nova_cidade'),
-    path('estados/novo/', cadastra_estado, name='novo_estado')
+    path('estados/novo/', cadastra_estado, name='novo_estado'),
+    path('materiais/', list_m, name='lista_material'),
+    path('materiais/novo', cadastra_material, name='novo_material'),
+    path('materiais/tipo/novo', cad_tipo_mat, name='novo_tipo_material')
 ]
