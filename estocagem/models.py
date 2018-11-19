b01f25eea717a3950bf5d36b86b7c34a7c27f38e
Paulo Vinicius Baleeiro Ferreira <paulo.baleeiro@birmind.com.br>
2018-11-19 01:02:46 -0200

    Conserto de model e criação de model customizada

--- estocagem/models.py
+++ estocagem/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 from core.models import Material
+from django.utils.six import with_metaclass
+
+
+class UpperCaseCharField(models.CharField):
+
+    def __init__(self, *args, **kwargs):
+        super(UpperCaseCharField, self).__init__(*args, **kwargs)
+
+    def pre_save(self, model_instance, add):
+        value = getattr(model_instance, self.attname, None)
+        if value:
+            value = value.upper()
+            setattr(model_instance, self.attname, value)
+            return value
+        else:
+            return super(UpperCaseCharField, self).pre_save(model_instance, add)
 
 
 class PosiçãodeEstocagem(models.Model):
@@ -8,18 +24,18 @@ class PosiçãodeEstocagem(models.Model):
              ('3', 'PEPS'),
              ('4', 'UEPS')
                )
-    endereço_setor = models.CharField(max_length=8, primary_key=True, verbose_name="endereço do setor",
+    endereco_setor = UpperCaseCharField(max_length=8, unique=True, verbose_name="endereço do setor",
                                       help_text="Atenção, valor deve ser único")
-    nome_posição = models.CharField(max_length=20, verbose_name='nome/descrição do endereço')
+    nome_posicao = models.CharField(max_length=20, verbose_name='nome/descrição do endereço')
     tipo_saida= models.CharField(max_length=2, choices=TIPOS, default=1, verbose_name='tipo de saída do material')
     material = models.ForeignKey(Material, on_delete=models.PROTECT, null=True)
 
     class Meta:
         verbose_name = 'Posição de estocagem'
         verbose_name_plural = 'Posições de estocagem'
-        unique_together = ['endereço_setor', 'material']
+        unique_together = ['endereco_setor', 'material']
 
     def __str__(self):
-        return self.nome_posição
+        return self.nome_posicao
 
 
