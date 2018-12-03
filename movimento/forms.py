d7c1e618873a90775436658dc9fc90ec1f9634b6
Paulo Vinicius Baleeiro Ferreira <paulo.baleeiro@birmind.com.br>
2018-12-03 00:06:33 -0200

    adição de forms para movimentações

--- movimento/forms.py
+++ movimento/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Movimento, MovimentoMaterial, RequisicaoMaterial, Requisicao
+from .models import Movimento, MovimentoMaterial, RequisicaoMaterial, Requisicao, Devolucao, ItemDevolucao
 from core.models import Material
 
 
@@ -15,3 +15,25 @@ class RequisiçãoItensForm(ModelForm):
         model = RequisicaoMaterial
         exclude = ['requisicao']
 
+
+class DevolucaoForm(ModelForm):
+    class Meta:
+        model = Devolucao
+        fields = ['usuario']
+
+
+class DevolucaoItemForm(ModelForm):
+    class Meta:
+        model = ItemDevolucao
+        exclude = ['devolução']
+
+
+class MovimentoForm(ModelForm):
+    class Meta:
+        model = Movimento
+
+
+class MovimentoItemForm(ModelForm):
+    class Meta:
+        model = MovimentoMaterial
+        exclude = ['movimento']
