fb62cd1997197c9ec1d2af81ba6bf51cee54e6a5
Paulo Vinicius Baleeiro Ferreira <paulo.baleeiro@birmind.com.br>
2018-12-03 00:07:48 -0200

    adicioando urls para devoluções

--- movimento/urls.py
+++ movimento/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import requisita_material
+from .views import requisita_material, lista_requisicoes_nao_atendidas as nao_atendidas, devolve_material
 
 urlpatterns = [
     path('requisita/', requisita_material, name='requisicao'),
     path('requisita/<int:id>', requisita_material, name='requisicao'),
+    path('requisita/lista-nao', nao_atendidas, name='nao_atendidas'),
+    path('devolucao/', devolve_material, name='devolucao'),
+    path('devolucao/<int:id>', devolve_material, name='devolucao'),
 ]
