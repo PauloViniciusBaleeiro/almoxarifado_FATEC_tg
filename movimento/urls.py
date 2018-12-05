e111ff4ceef85f02b7a32ba10b06a278c9a62452
Paulo Vinicius Baleeiro Ferreira <paulo.baleeiro@birmind.com.br>
2018-12-05 21:05:58 -0200

    fix url

--- movimento/urls.py
+++ movimento/urls.py
@@ -3,7 +3,7 @@ from .views import requisita_material, lista_requisicoes_nao_atendidas as nao_at
 
 urlpatterns = [
     path('requisita/', requisita_material, name='requisicao'),
-    path('requisita/<int:id>', requisita_material),
+    path('requisita/<int:id>', requisita_material, name='requisita_new'),
     path('requisita/lista-nao', nao_atendidas, name='nao_atendidas'),
     path('devolucao/', devolve_material, name='devolucao'),
     path('devolucao/<int:id>', devolve_material, name='devolucao'),
