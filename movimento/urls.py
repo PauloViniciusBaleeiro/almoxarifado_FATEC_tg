c37de60305360edb0d10d4495d2d77e8bcde637a
Paulo Vinicius Baleeiro Ferreira <paulo.baleeiro@birmind.com.br>
2018-12-05 21:22:29 -0200

    adicionado url para decremento

--- movimento/urls.py
+++ movimento/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import requisita_material, lista_requisicoes_nao_atendidas as nao_atendidas, devolve_material, descarte
+from .views import (requisita_material, lista_requisicoes_nao_atendidas as nao_atendidas, devolve_material,
+                    descarte, decremento)
 
 urlpatterns = [
     path('requisita/', requisita_material, name='requisicao'),
@@ -7,5 +8,6 @@ urlpatterns = [
     path('requisita/lista-nao', nao_atendidas, name='nao_atendidas'),
     path('devolucao/', devolve_material, name='devolucao'),
     path('devolucao/<int:id>', devolve_material, name='devolucao'),
-    path('descarte/', descarte, name='descarte')
+    path('descarte/', descarte, name='descarte'),
+    path('decremento/', decremento, name='decremento'),
 ]
