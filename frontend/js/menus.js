4fecd5a0c78b6979b37957619cba198f50c0e994
Paulo Vinicius Baleeiro Ferreira <paulo@jumpi.com.br>
2019-03-25 22:50:32 -0300

    add files to master

---
+++ frontend/js/menus.js
@@ -0,0 +1,21 @@
+$(function () {    
+    
+    $('#linkMenuRequisitarMaterial').attr('href', '001_RequisitarMaterial.html');
+    $('#linkMenuRequisicaoExtra').attr('href', '001_RequisicaoExtra.html');
+    $('#linkMenuLocalizarRequisicao').attr('href', '001_LocalizarRequisicao.html');
+    $('#linkMenuEntradaMaterialEstoque').attr('href', '002_EntradaMaterialEstoque.html');
+    $('#linkMenuDecrementoDescarte').attr('href', '003_DecrementoDescarte.html');
+    $('#linkMenuCadastroFabricante').attr('href', '004_CadastroFabricante.html');
+    $('#linkMenuCadastroMaterial').attr('href', '005_CadastroMaterial.html');
+    $('#linkMenuCadastroUsuario').attr('href', '006_CadastroUsuario.html');
+    $('#linkMenuConsultaRequisicao').attr('href', '007_ConsultaRequisicao.html');
+    $('#linkMenuConsultaFabricante').attr('href', '008_ConsultaFabricante.html');
+    $('#linkMenuDecrementoEstoque').attr('href', '009_DecrementoEstoque.html');
+    $('#linkMenuDevolverMaterial').attr('href', '010_DevolverMaterial.html');
+    $('#linkMenuEditarFabricante').attr('href', '011_EditarFabricante.html');
+    $('#linkMenuGerarEtiqueta').attr('href', '012_GerarEtiquetas.html');
+    $('#linkMenuInventario').attr('href', '013_Inventario.html');
+    $('#linkMenuLancarContagem').attr('href', '014_LancarContagem.html');
+    $('#linkMenuPosicaoEstocagem').attr('href', '015_PosicaoEstocagem.html');
+    
+});
