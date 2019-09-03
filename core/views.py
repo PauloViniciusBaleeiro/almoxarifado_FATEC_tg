56c1b439fb5636bc8c9d779fa4e5a6c0fce4befb
Paulo Vinicius Baleeiro Ferreira <paulo@jumpi.com.br>
2019-09-02 22:23:53 -0300

    conclusão do login

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.decorators import login_required
 from .forms import (FabricanteForms, CidadeForm, EstadoForms, ContatoForm, MaterialForm, TipodeMaterialForm,
                     EntradaMaterialForm)
@@ -7,8 +7,20 @@ from .models import Fabricante, Contato, Material
 from movimento.models import Requisicao
 from movimento.models import Movimento, MovimentoMaterial
 
+
 def landing(request):
     return render(request, 'landing_page.html')
+
+
+def login(request):
+    username = request.post['username']
+    password = request.post['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('home')
+    else:
+        return redirect('landing')
 
 
 @login_required
