1320eb538b961a882d874d4e4553def1c880e004
Paulo Vinicius Baleeiro Ferreira <paulo@jumpi.com.br>
2019-09-02 23:22:49 -0300

    correção chamada de estáticos

--- TG_Fatec/settings.py
+++ TG_Fatec/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend')
 STATICFILES_DIRS = (
     os.path.join(FRONTEND_DIR, 'static'),
 )
